0c656b9d49d9a0abf07af9ec07a06b220b01b98d
tabirako <79563568+tabirako@users.noreply.github.com>
2025-03-02 12:59:34 +0800

    Update language.js
    edit jp about me

--- js/language.js
+++ js/language.js
@@ -39,12 +39,12 @@ function showContent() {
     lang = getCookie("language");
     if (lang === "ja") {
         document.getElementById("headtitle").innerHTML = "KCの随想録";
-        document.getElementById("aboutme").innerHTML = "私は<strong id='usernameid'>KC(KirisameCirno)</strong>と申します。<br>音楽全般、ゲームとアニメ関係のが好きです。<br>色んな飲み物と料理も好きです。アマチュア無線を扱っている人（第三級アマチュア無線技士+台湾三等業餘無線電人員)<br>ピアノ、アコーディオンとフルート奏者であります。喉歌もできるです<br>ご覧いただきありがとうございます！";
+        document.getElementById("aboutme").innerHTML = "私は<strong id='usernameid'>KC(KirisameCirno)</strong>と申します。<br>音楽全般、ゲームとアニメ関係のが好きです。<br>色んな料理と飲み物、特に台湾茶が好きです。アマチュア無線を扱っている人（第三級アマチュア無線技士+台湾三等業餘無線電人員)<br>ピアノ、アコーディオンとフルート奏者であります。喉歌もできるです<br>ご覧いただきありがとうございます！";
         document.getElementById("whatisthis").innerHTML = "このサイトについて";
         document.getElementById("webinfo").innerHTML = "できるだけ簡単な方法で個人ページを作りました！";
         document.getElementById("madeby").innerHTML = "作成者：ItzPeto";
     } else if (lang === "zh") {
-        document.getElementById("headtitle").innerHTML = "KC的隨草";
+        document.getElementById("headtitle").innerHTML = "KC隨草";
         document.getElementById("aboutme").innerHTML = "我叫做<strong id='usernameid'>KC(KirisameCirno)</strong>。<br>興趣是音樂，遊戲，動畫和漫畫。<br>喜歡各種飲料和料理，尤其是台灣茶，同時是業餘無線電操作者（現在是台灣三等業餘無線電人員+日本第三級アマチュア無線技士）<br>能夠彈奏鋼琴，手風琴和一點點的長笛<br>感謝您的來訪！";
         document.getElementById("whatisthis").innerHTML = "關於這個網站";
         document.getElementById("webinfo").innerHTML = "試圖用簡單的方法製作了網站";
@@ -54,7 +54,7 @@ function showContent() {
         document.getElementById("aboutme").innerHTML = "what the fuck is oatmeal";
         document.getElementById("whatisthis").innerHTML = "where do i get oatmeal?";
         document.getElementById("webinfo").innerHTML = "i have no fucking idea";
-        document.getElementById("madeby").innerHTML = "uhh helo itzpeto made this shit";
+        document.getElementById("madeby").innerHTML = "Itzpeto 製造";
     }
 }
 
